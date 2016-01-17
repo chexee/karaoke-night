87677061ba350627996f54c7b0070c392af6a16f
chexee <chelsea@vouch.com>
2016-01-16 22:21:54 -0800

    change prod port

--- server.prod.js
+++ server.prod.js
@@ -14,7 +14,7 @@ new WebpackDevServer(webpack(config), { // Start a server
   lazy: true,
   historyApiFallback: true,
   quiet: true // Without logging
-}).listen(3000, '0.0.0.0', function (err, result) {
+}).listen(process.env.PORT, '0.0.0.0', function (err, result) {
   if (err) {
     console.log(err);
   } else {
