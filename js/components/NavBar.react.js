d6973b02513fa1dcc25c526716957944f83bd3c6
chexee <chelsea@vouch.com>
2016-01-18 14:13:15 -0800

    Navigate tabs with react router

--- js/components/NavBar.react.js
+++ js/components/NavBar.react.js
@@ -6,6 +6,7 @@
  */
 
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 
 export default class NavBar extends Component {
   componentDidMount() {
@@ -20,18 +21,18 @@ export default class NavBar extends Component {
     return (
       <div className="mt2 bottom-0 col-12 bg-purple-2 center fixed top-shadow">
         <div className="clearfix" id="nav-tabs">
-          <a data-nav-tab="songs" href="/songs" className="col col-4 py1 border-top border-3 border-purple-3">
+          <Link data-nav-tab="songs" to="/songs" className="col col-4 py1 border-top border-3 border-purple-3">
             <div><div className="icon-disc icon-20 white"></div></div>
             <span className="h6 caps white bold">Songs</span>
-          </a>
-          <a data-nav-tab="songbooks" href="/songbooks" className="col col-4 py1 border-top border-3 border-purple-3">
+          </Link>
+          <Link data-nav-tab="songbooks" to="/songbooks" className="col col-4 py1 border-top border-3 border-purple-3">
             <div><div className="icon-book icon-20 white"></div></div>
             <span className="h6 caps white bold">Songbooks</span>
-          </a>
-          <a data-nav-tab="favorites" href="/favorites" className="col col-4 py1 border-top border-3 border-purple-3">
+          </Link>
+          <Link data-nav-tab="favorites" to="/favorites" className="col col-4 py1 border-top border-3 border-purple-3">
             <div><div className="icon-ribbon icon-20 white"></div></div>
             <span className="h6 caps white bold">Favorites</span>
-          </a>
+          </Link>
         </div>
       </div>
     )
